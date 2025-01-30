69ededa0aca27b3983ac97bc55ef03e3b60ffe7a
Mateusz <chudy3133@gmail.com>
2025-01-30 09:25:59 +0100

    Fix API routes for dynamic rendering and Vercel mess 12345678

--- src/app/[lang]/(dashboard)/layout.tsx
+++ src/app/[lang]/(dashboard)/layout.tsx
@@ -2,6 +2,10 @@
 
 import { useEffect } from 'react';
 import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -31,5 +35,25 @@ export default function DashboardLayout({ children, params }: DashboardLayoutPro
     initUser();
   }, [user]);
 
-  return <>{children}</>;
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href={`/${params.lang}`}
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold">NGO-Platform</span>
+        </Link>
+        <Menu lang={params.lang} />
+      </div>
+      
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
 }
